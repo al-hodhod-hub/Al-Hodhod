9acb7efb27f347aa21783b56e4b0943a3949e662
al-hodhod-hub <M.s.rawaa123@gmail.com>
2025-12-17 22:17:38 +0400

    Remove metadata export from layout.js
    Removed metadata export for title and description.

--- app/app/layout.js
+++ app/app/layout.js
@@ -1,10 +1,4 @@
 import "./globals.css";
-
-export const metadata = {
-  title: "Al Hodhod | الهدهد",
-  description: "Car rental in Muscat"
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
